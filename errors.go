5f674e968e459321c7c0e34587c455f1afb7057e
brianvoe <brian@webiswhatido.com>
2019-07-06 00:41:23 -0500

    errors - added invalid token

--- errors.go
+++ errors.go
@@ -3,6 +3,10 @@ package sjwt
 import "errors"
 
 var (
+	// ErrTokenInvalid is an error string clarifying
+	// the provided token is an invalid format
+	ErrTokenInvalid = errors.New("Token is invalid")
+
 	// ErrTokenHasExpired is an error string clarifying
 	// the current unix timestamp has exceed the exp unix timestamp
 	ErrTokenHasExpired = errors.New("Token has expired")

c035e3ea1468013443760ef897fecab17b28322d
brianvoe <brian@webiswhatido.com>
2019-07-06 00:41:13 -0500

    claims public - minor updates and added more tests

--- claims_pub_test.go
+++ claims_pub_test.go
@@ -9,8 +9,11 @@ func TestClaims(t *testing.T) {
 	claims.Add("stringbool", "true")
 	claims.Add("string", "hello world")
 	claims.Add("intstring", 8675309)
-	claims.Add("floatstring", 8675309.69)
+	claims.Add("float32string", float32(86753.09))
+	claims.Add("float64string", float64(86753.09))
 	claims.Add("int", 8675309)
+	claims.Add("uintint", uint(8675309))
+	claims.Add("floatint", 86753.09)
 	claims.Add("stringint", "8675309")
 	claims.Add("float", 8675309.69)
 	claims.Add("stringfloat", "8675309.69")
@@ -51,15 +54,27 @@ func TestClaims(t *testing.T) {
 	if intstring != "8675309" {
 		t.Error("intstring claim is incorrect, got: ", intstring)
 	}
-	floatstring := claims.GetStr("floatstring")
-	if floatstring != "8675309.69" {
-		t.Error("floatstring claim is incorrect, got: ", floatstring)
+	float32string := claims.GetStr("float32string")
+	if float32string != "86753.09" {
+		t.Error("float32string claim is incorrect, got: ", float32string)
+	}
+	float64string := claims.GetStr("float64string")
+	if float64string != "86753.09" {
+		t.Error("float64string claim is incorrect, got: ", float64string)
 	}
 
 	// Integer
 	int := claims.GetInt("int")
 	if int != 8675309 {
 		t.Error("int claim is incorrect, got: ", int)
+	}
+	uintint := claims.GetInt("uintint")
+	if uintint != 8675309 {
+		t.Error("uintint claim is incorrect, got: ", uintint)
+	}
+	floatint := claims.GetInt("floatint")
+	if floatint != 86753 {
+		t.Error("floatint claim is incorrect, got: ", floatint)
 	}
 	stringint := claims.GetInt("stringint")
 	if stringint != 8675309 {

43cf63e2d8445c87f4c7f8da44bdbfbba0bf60cd
brianvoe <brian@webiswhatido.com>
2025-11-09 11:53:30 -0600

    errors - lint error string starts with lowercase

--- errors.go
+++ errors.go
@@ -5,21 +5,21 @@ import "errors"
 var (
 	// ErrNotFound is an error string clarifying
 	// that the attempted key does not exist in the claims
-	ErrNotFound = errors.New("Claim key not found in claims")
+	ErrNotFound = errors.New("claim key not found in claims")
 
 	// ErrClaimValueInvalid is an error string clarifying
 	// that the attempt to retrieve a value could not be properly converted
-	ErrClaimValueInvalid = errors.New("Claim value invalid")
+	ErrClaimValueInvalid = errors.New("claim value invalid")
 
 	// ErrTokenInvalid is an error string clarifying
 	// the provided token is an invalid format
-	ErrTokenInvalid = errors.New("Token is invalid")
+	ErrTokenInvalid = errors.New("token is invalid")
 
 	// ErrTokenHasExpired is an error string clarifying
 	// the current unix timestamp has exceed the exp unix timestamp
-	ErrTokenHasExpired = errors.New("Token has expired")
+	ErrTokenHasExpired = errors.New("token has expired")
 
 	// ErrTokenNotYetValid is an error string clarifying
 	// the current unix timestamp has not exceeded the nbf unix timestamp
-	ErrTokenNotYetValid = errors.New("Token is not yet valid")
+	ErrTokenNotYetValid = errors.New("token is not yet valid")
 )
